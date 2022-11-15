9907a4e9d531ece9c46ed8e4c23dc90657ccdf72
marisasaunders <marisaksaunders@gmail.com>
2022-11-14 21:48:46 -0500

    commit tree f806285d74a026d7073dffe730309f856b213bde

---
+++ db/migrate/20221115023724_create_spices.rb
@@ -0,0 +1,14 @@
+class CreateSpices < ActiveRecord::Migration[6.1]
+  def change
+    create_table :spices do |t|
+      t.string :title
+      t.string :image
+      t.string :description
+      t.string :notes
+      t.float :rating
+      t.string :species
+
+      t.timestamps
+    end
+  end
+end
